afec35fc88d5e9b367f17f6adebd1ecb4652117f
Kristal0024 <kristalstha20@gmail.com>
2024-12-30 21:04:52 +0545

    mobile responsive

--- landing.js
+++ landing.js
@@ -1,5 +1,9 @@
 const hamburger=document.querySelector(".menu");
 const list=document.querySelector(".nav ul");
+const facebook=document.getElementById("fbimage");
+const insta=document.getElementById("insta");
+const youtube=document.getElementById("youtube");
+const twitter=document.getElementById("twitter");
 
 hamburger.addEventListener("click",()=>{
     if(list.style.display==="none"){
@@ -8,3 +12,15 @@ hamburger.addEventListener("click",()=>{
         list.style.display="none";
     }
 });
+facebook.addEventListener("click",()=>{
+    window.open("https://www.facebook.com/wiped.person.3");
+});
+insta.addEventListener("click",()=>{
+    alert("didn't have instagram account");
+});
+youtube.addEventListener("click",()=>{
+    alert("didn't have youtube");
+});
+twitter.addEventListener("click",()=>{
+    alert("didn't have twitter");
+})
